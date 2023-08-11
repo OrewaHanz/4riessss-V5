8d41a3adf919a879853591b3376c2206ae745190
Hanz Bobot <132050914+OrewaHanz@users.noreply.github.com>
2023-08-12 02:48:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ global.author = 'mochiboba'
 global.alpiskey = 'YOUR_APIKEY_HERE' //daftar sendiri jika key habis https://alpis.eu.org
 
 global.btc  = 'putxd' //Daftar terlebih dahulu https://api.botcahx.live
-global.lann = 'putxd' //Note Key Ini Hanya Bertahan Selama 4 Day Sejak Config.js update! lebih baik register di https://api.betabotz.me
+global.lann = 'exP6GSZj' //Note Key Ini Hanya Bertahan Selama 4 Day Sejak Config.js update! lebih baik register di https://api.betabotz.me
 global.APIs = { 
   tio: 'https://api.botcahx.live',
   alpis: 'https://alpis.eu.org'
